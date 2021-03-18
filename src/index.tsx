be2eed072d8845f837700c953be36c3aa456dc11
breezertwo <brechtelandreas@gmail.com>
2021-03-18 01:06:32 +0100

    Basic react typescript env

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './assets/index.scss';
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
